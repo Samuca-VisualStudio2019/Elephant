34b695d1d7f5a1a25f02ec22b3605f5615f89b30
Samuca-VisualStudio2019 <samucateclas@gmail.com>
2021-04-05 17:40:50 -0300

    Class Elephant Created

---
+++ AppElephant/AppElephant/Elephant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppElephant
+{
+    class Elephant
+    {
+        public int EarSize;
+        public double Name;
+    }
+}
